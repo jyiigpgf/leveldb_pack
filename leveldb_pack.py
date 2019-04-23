127e4c43a43b2477c8d950be6c41a6c8ca126a53
jing <>
2019-04-23 17:49:48 +0800

    no message

--- leveldb_pack.py
+++ leveldb_pack.py
@@ -4,32 +4,51 @@ import plyvel
 
 db = None
 
+DICT_FORMAT_C = b'd'
+STR_FORMAT_C = b's'
+INT_FORMAT_C = b'i'
+LIST_FORMAT_C = b'l'
 
-class tType():
+
+class tType:
+    def __init__(self, name):
+        if type(name) is str:
+            self.name = name
+        elif type(name) is bytes:
+            self.name = name.decode('utf-8')
+        else:
+            raise TypeError(name)
+
     def _py_value(self, value):
-        if value[0] == int.from_bytes(b's', byteorder='little'):
+        if value[0] == int.from_bytes(STR_FORMAT_C, byteorder='little'):
             value = value[1:].decode('utf-8')
-        elif value[0] == int.from_bytes(b'i', byteorder='little'):
+        elif value[0] == int.from_bytes(INT_FORMAT_C, byteorder='little'):
             value = int.from_bytes(value[1:], byteorder='little', signed=True)
         return value
 
     def _byte_value(self, value):
         if type(value) is str:
-            value = b's' + value.encode('utf-8')
+            value = STR_FORMAT_C + value.encode('utf-8')
         elif type(value) is int:
-            value = b'i' + value.to_bytes((value.bit_length() + 7) // 8 + 1, byteorder='little', signed=True)
+            value = INT_FORMAT_C + value.to_bytes((value.bit_length() + 7) // 8 + 1, byteorder='little', signed=True)
         else:
-            raise ValueError(value)
+            raise TypeError(value)
         return value
 
     def _wrap_key(self, key):
         return (self.name + '_' + key).encode('utf-8')
 
+    def clear(self):
+        with db.write_batch(transaction=True) as wb:
+            wb.delete(self.name.encode('utf-8'))
+            for key, value in db.iterator(prefix=(self.name + '_').encode('utf-8')):
+                wb.delete(key)
+
 
 class tList(tType):
     def __init__(self, name, _list=None):
-        print('__init__')
-        self.name = name
+        super().__init__(name)
+        db.put(self.name.encode('utf-8'), LIST_FORMAT_C)
         if _list is None:
             return
         count = self._get_count()
@@ -38,37 +57,54 @@ class tList(tType):
         index = count - 1
         with db.write_batch(transaction=True) as wb:
             for item in _list:
-                if item is None:
-                    raise ValueError(item)
                 index += 1
                 key = self._wrap_key(str(index))
-                wb.put(key, self._byte_value(item))
+                if item is None:
+                    raise ValueError(item)
+                elif type(item) is list:
+                    tList(key, item)
+                elif type(item) is dict:
+                    tDict(key, item)
+                else:
+                    wb.put(key, self._byte_value(item))
             self._set_count(count + len(_list), wb)
 
     def __len__(self):
-        print('__init__')
         return self._get_count()
 
     def __getitem__(self, index):
-        print('__getitem__')
         if type(index) is int:
             key = self._wrap_key(str(index))
             value = db.get(key)
             if value is None:
                 raise IndexError(index)
             else:
-                return self._py_value(value)
+                if value == LIST_FORMAT_C:
+                    return tList(key)
+                elif value == DICT_FORMAT_C:
+                    return tDict(key)
+                else:
+                    return self._py_value(value)
         else:
             raise TypeError(index)
 
     def __setitem__(self, index, value):
-        print('__setitem__')
         count = self._get_count()
         if type(index) is int:
             if index < count:
                 key = self._wrap_key(str(index))
-                value = self._byte_value(value)
-                db.put(key, value)
+                old_value = db.get(key)
+                if old_value == LIST_FORMAT_C:
+                    tList(key).clear()
+                elif old_value == DICT_FORMAT_C:
+                    tDict(key).clear()
+                if type(value) is list:
+                    tList(key, value)
+                elif type(value) is dict:
+                    tDict(key, value)
+                else:
+                    value = self._byte_value(value)
+                    db.put(key, value)
             else:
                 raise IndexError(index)
         else:
@@ -78,17 +114,35 @@ class tList(tType):
         count = self._get_count()
         last_index = self._wrap_key(str(count - 1))
         value = db.get(last_index)
-        with db.write_batch(transaction=True) as wb:
-            wb.delete(self._wrap_key(str(count - 1)))
-            self._set_count(count - 1, wb)
-        return self._py_value(value)
+        if value == LIST_FORMAT_C:
+            with db.write_batch(transaction=True) as wb:
+                tList(last_index).clear()
+                self._set_count(count - 1, wb)
+        elif value == DICT_FORMAT_C:
+            with db.write_batch(transaction=True) as wb:
+                tDict(last_index).clear()
+                self._set_count(count - 1, wb)
+        else:
+            with db.write_batch(transaction=True) as wb:
+                wb.delete(self._wrap_key(str(count - 1)))
+                self._set_count(count - 1, wb)
+            return self._py_value(value)
 
     def append(self, value):
         count = self._get_count()
         index = count
-        with db.write_batch(transaction=True) as wb:
-            wb.put(self._wrap_key(str(index)), self._byte_value(value))
-            self._set_count(count + 1)
+        if type(value) is list:
+            with db.write_batch(transaction=True) as wb:
+                tList(self._wrap_key(str(index)), value)
+                self._set_count(count + 1)
+        elif type(value) is dict:
+            with db.write_batch(transaction=True) as wb:
+                tDict(self._wrap_key(str(index)), value)
+                self._set_count(count + 1)
+        else:
+            with db.write_batch(transaction=True) as wb:
+                wb.put(self._wrap_key(str(index)), self._byte_value(value))
+                self._set_count(count + 1)
 
     def _get_count(self):
         key = self.name + '_' + 'count'
@@ -107,52 +161,56 @@ class tList(tType):
 
 class tDict(tType):
     def __init__(self, name, _dict=None):
-        print('__init__')
-        self.name = name
-        if _dict is None:
-            return
-        tile_dict = self._dict_tile(self.name, _dict)
+        super().__init__(name)
+        db.put(self.name.encode('utf-8'), DICT_FORMAT_C)
+        if _dict is not None:
+            self._set_dict(self.name, _dict)
+        # TODO: else
+
+    def _set_dict(self, key, _dict):
+        tile_dict = self._dict_tile(key, _dict)
         with db.write_batch(transaction=True) as wb:
             for k, v in tile_dict.items():
                 wb.put(k.encode('utf-8'), v)
 
     def _dict_tile(self, prefix_key, _dict):
-        rtv = {}
-        print(type(_dict))
+        rtv = {prefix_key: DICT_FORMAT_C}
         for k, v in _dict.items():
             if type(v) is dict:
-                rtv[prefix_key + '_' + k] = b'd'
                 rtv.update(self._dict_tile(prefix_key + '_' + k, v))
             else:
                 rtv[prefix_key + '_' + k] = self._byte_value(v)
         return rtv
 
     def __setitem__(self, key, value):
-        print('__setitem__')
-        key = self._wrap_key(key)
-        value = self._byte_value(value)
-        db.put(key, value)
+        old_value = db.get(self._wrap_key(key))
+        if old_value == DICT_FORMAT_C:
+            self[key].clear()
+
+        if type(value) is dict:
+            self._set_dict(self.name + '_' + key, value)
+        else:
+            key = self._wrap_key(key)
+            value = self._byte_value(value)
+            db.put(key, value)
 
     def __getitem__(self, key):
-        print('__getitem__')
         key = self._wrap_key(key)
         value = db.get(key)
         if value is None:
             raise KeyError(key)
         else:
-            if value == b'd':
-                return tDict(key.decode('utf-8'))
+            if value == DICT_FORMAT_C:
+                return tDict(key)
             else:
                 return self._py_value(value)
 
     def __contains__(self, key):
-        print('__contains__')
         key = self._wrap_key(key)
         value = db.get(key)
-        return value != None
+        return value is not None
 
     def pop(self, key, d=None):
-        print('pop')
         key = self._wrap_key(key)
         value = db.get(key)
         if value is None:
@@ -160,6 +218,9 @@ class tDict(tType):
                 raise KeyError(key)
             else:
                 return d
+        elif value == DICT_FORMAT_C:
+            # TODO: return dict?
+            tDict(key).clear()
         else:
             value = self._py_value(value)
             db.delete(key)
@@ -170,11 +231,21 @@ if __name__ == '__main__':
 
     db = plyvel.DB('./testleveldb/', create_if_missing=True)
 
-    with db.write_batch() as wb:
+    with db.write_batch(transaction=True) as wb:
         for key, value in db:
             wb.delete(key)
 
-    test_data = tList("test_list", [1, 2, 3, 4, 5, 6, 7])
+    test_data = tList('TestList', ['a', {'b': 'hello'}])
+    print(type(test_data[1]))
+    test_data[0] = [1,2,3]
+    print(type(test_data[0]))
+    test_data[0] = {'a': 'world'}
+    print(type(test_data[0]))
+    test_data[0] = 'a'
+    print(test_data[0])
+    test_data.append({'c': 'world'})
+    print(type(test_data[2]))
+
 
     for key, value in db:
         print(key, value)
--- leveldb_pack_test.py
+++ leveldb_pack_test.py
@@ -11,7 +11,7 @@ class TestTDict(unittest.TestCase):
     def test_init(self):
         leveldb_pack.db = plyvel.DB('./testleveldb/', create_if_missing=True)
 
-        with leveldb_pack.db.write_batch() as wb:
+        with leveldb_pack.db.write_batch(transaction=True) as wb:
             for key, value in leveldb_pack.db:
                 wb.delete(key)
 
@@ -22,7 +22,7 @@ class TestTDict(unittest.TestCase):
         leveldb_pack.db.close()
 
     def test_dict(self):
-        test_data = tDict('test_dict', {
+        test_data = tDict('TestDict', {
             'a': 4,
             'b': 'abc',
             'c': {
@@ -37,8 +37,15 @@ class TestTDict(unittest.TestCase):
         # __setitem__
         test_data['a'] = 10
         self.assertEqual(test_data['a'], 10)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             test_data['a'] = None
+        test_data['g'] = {
+            "d2": "hello world"
+        }
+        self.assertEqual(test_data['g']['d2'], 'hello world')
+        test_data['g'] = 'hello'
+        with self.assertRaises(TypeError):
+            test_data['g']['d2']
 
         # __getitem__
         self.assertEqual(test_data['a'], 10)
@@ -55,32 +62,43 @@ class TestTDict(unittest.TestCase):
         with self.assertRaises(KeyError):
             print(test_data['b'])
         self.assertEqual(test_data.pop('b', 'hello'), 'hello')
+        test_data.pop('c')
 
         # __init__
-        test_data = tDict('test_dict')
+        test_data = tDict('TestDict')
         self.assertEqual(test_data['a'], 10)
 
+        # clear
+        test_data.clear()
+
     def test_list(self):
-        test_data = tList('test_list', ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+        test_data = tList('TestList', ['a', 'b', 'c', 'd', 'e', 'f', ['g1', 'g2']])
         with self.assertRaises(TypeError):
-            tList('test_list', ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
-        test_data = tList('test_list')
+            tList('TestList', ['a', 'b', 'c', 'd', 'e', 'f', ['g1', 'g2']])
+        test_data = tList('TestList')
 
         # __len__
         self.assertEqual(len(test_data), 7)
 
         # __getitem__
         self.assertEqual(test_data[3], 'd')
+        self.assertEqual(type(test_data[6]), tList)
 
         # __setitem__
         test_data[0] = 'c'
         self.assertEqual(test_data[0], 'c')
         with self.assertRaises(IndexError):
             test_data[10000] = 'c'
+        test_data[6] = 'g'
+        self.assertEqual(test_data[6], 'g')
 
-        # append
+        # append & pop
         test_data.append('hello')
-
-        # pop
         self.assertEqual(test_data.pop(), 'hello')
+        test_data.append(['h1', 'h2', 'h3', 'h4'])
+        self.assertEqual(type(test_data[7]), tList)
+        test_data.pop()
+
+        # clear
+        test_data.clear()
 
