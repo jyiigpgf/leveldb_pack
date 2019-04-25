c7645efd6340daf531229f0f638fc5f98748ec19
jing <>
2019-04-25 17:00:41 +0800

    readme

--- leveldb_pack.py
+++ leveldb_pack.py
@@ -2,15 +2,6 @@
 
 import plyvel
 
-# TODO: pop TList TDict是否需要返回
-# TODO: 初始化已有数据的TList TDict报出异常
-# TODO: list实现加法符号
-# TODO: 检查递归调用
-# TODO: TDict key支持非字符串类型
-# TODO: TList 实现反向index
-# TODO: TList pop接口，支持传入index
-# TODO: TList 支持__delitem__
-
 db = None
 
 DICT_FORMAT_C = b'd'
